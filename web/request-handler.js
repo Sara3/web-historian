97198d3270d8c76cfc336e1efede92d6470bf3db
Hack Reactor Student <student@hackreactor.com>
2017-06-09 19:24:49 -0700

    handleRequest serves html from files

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,24 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelp = require('./http-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  //console.log('req', req, 'res', res);
+ 
+  fs.readFile(archive.paths.siteAssets + '/index.html', 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(data);
+    }
+  });
+  res.end();
 };
+
+//httpHelp.serveAssets('res', 'pagedata', 'callback function');
+//load html onto server (fs.readFile?)
+
+//If typeof request is GET
+  //load "loading" HTML
